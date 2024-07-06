019225e1b22a1586c00bf9f22012740fe06bf465
nyx6965 <72759681+nyx6965@users.noreply.github.com>
2024-07-06 20:19:42 +0300

    Added nix shell

---
+++ shell.nix
@@ -0,0 +1,20 @@
+
+# shell.nix
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    packages = [
+     pkgs.gdb
+
+    pkgs.cmake
+    pkgs.pkg-config
+    pkgs.meson
+    pkgs.ninja
+
+    # Add some libraries
+    pkgs.boost
+    pkgs.fmt
+];
+  }
+
